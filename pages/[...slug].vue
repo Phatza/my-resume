c435d6cf5a5247bd422a0f9c59562a521a3fc9f8
phatza <haphuoc@gmail.com>
2022-06-09 21:59:16 +0200

    Add slot #not-found to handle slug not founded or that doesn't exist anymore

--- pages/[...slug].vue
+++ pages/[...slug].vue
@@ -1,5 +1,29 @@
 <template>
   <main>
-    <ContentDoc />
+    <ContentDoc>
+      <template #not-found>
+        <div id="PageNotFound">
+          <h1>404</h1>
+          <p>
+            Page
+            <span>"{{ $route.params.slug[0] }}"</span>
+            not found
+          </p>
+          <a href="/">Go back to homepage</a>
+        </div>
+      </template>
+    </ContentDoc>
   </main>
 </template>
+
+<style lang="scss">
+#PageNotFound {
+  border: 2px solid #000000;
+  text-align: center;
+  padding-bottom: 1em;
+    span {
+      font-weight: bold;
+      text-transform: uppercase;
+    }
+}
+</style>
