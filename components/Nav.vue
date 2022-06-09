05bb5252d6e2a5dd647a8ffd0521e018f56aee44
phatza <haphuoc@gmail.com>
2022-06-09 21:53:43 +0200

    Create Nav components to display navigation

---
+++ components/Nav.vue
@@ -0,0 +1,13 @@
+<template>
+    <nav>
+        <ContentNavigation v-slot="{ navigation }">
+            <ul v-for="link of navigation" :key="link._path">
+                <li>
+                    <NuxtLink :to="link._path">
+                        {{ link.title }}
+                    </NuxtLink>
+                </li>
+            </ul>
+        </ContentNavigation>
+    </nav>
+</template>
