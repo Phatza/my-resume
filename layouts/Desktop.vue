b698ca75cd453db94e063613f21fa0a8dd4a8533
phatza <haphuoc@gmail.com>
2022-06-15 14:33:47 +0200

    Create: 2 layouts - mobile and desktop

---
+++ layouts/Desktop.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container md:grid-cols-2 mx-auto">
+        <header role="navigation">
+            <Nav />
+        </header>
+        <main>
+            Desktop LAYOUT
+            <slot />
+        </main>
+    </div>
+</template>
---
+++ layouts/Mobile.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="container grid-cols-1 mx-auto">
+        <header role="navigation">
+            <Nav />
+        </header>
+        <main>
+            Mobile LAYOUT
+            <slot />
+        </main>
+    </div>
+</template>
