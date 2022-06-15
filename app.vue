2ab9ba59282fbfc3240514a7288d795da16ae70b
phatza <haphuoc@gmail.com>
2022-06-15 14:34:46 +0200

    Add: layouts to app and logic to display the right one based on witdh

--- app.vue
+++ app.vue
@@ -1,6 +1,20 @@
 <template>
-  <div>
-    <Nav />
+  <NuxtLayout v-if="windowWidth > 768" name="desktop">
     <NuxtPage />
-  </div>
+  </NuxtLayout>
+  <NuxtLayout v-else name="mobile">
+    <NuxtPage />
+  </NuxtLayout>
 </template>
+
+<script setup>
+import { onMounted, ref } from 'vue'
+const windowWidth = ref(0);
+onMounted(() => {
+  // Watch window width to display the right layout
+  windowWidth.value = window.innerWidth;
+  window.addEventListener('resize', () => {
+    windowWidth.value = window.innerWidth;
+  });
+});
+</script>
