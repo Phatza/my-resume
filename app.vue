dfc8ed3e5db36c90152e35a4cde262350a8dec83
phatza <haphuoc@gmail.com>
2022-06-17 17:22:52 +0200

    Remove useless import and Add transition effect to pages

--- app.vue
+++ app.vue
@@ -8,7 +8,6 @@
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue'
 const windowWidth = ref(0);
 onMounted(() => {
   // Watch window width to display the right layout
@@ -18,3 +17,40 @@ onMounted(() => {
   });
 });
 </script>
+
+<style lang="scss">
+.page-enter-active,
+.page-leave-active {
+  opacity: 0;
+}
+.page-enter-to {
+  opacity: 0;
+  animation: slide-in 0.5s;
+}
+.page-leave-to {
+  opacity: 0;
+  animation: slide-out 0.5s;
+}
+/* Create the animation... */
+@keyframes slide-in {
+  from {
+    transform: translateX(100%);
+    opacity: 0;
+  }
+  to {
+    transform: translateX(0%);
+    opacity: 1;
+  }
+}
+@keyframes slide-out {
+  from {
+    transform: translateX(0%);
+    
+    opacity: 1;
+  }
+  to {
+    transform: translateX(-100%);
+    opacity: 0;
+  }
+}
+</style>
