59259ec88cd78684ad89e7f89a50d4b420055a1e
phatza <haphuoc@gmail.com>
2022-06-17 17:26:50 +0200

    Update: modify navigation links

--- components/Nav.vue
+++ components/Nav.vue
@@ -1,7 +1,10 @@
 <template>
     <nav>
-        <ul class="flex justify-evenly md:flex-col md:justify-center md:content-center">
-            <li>
+        <ul class="flex justify-center md:flex-col md:justify-center md:content-center">
+            <li class="px-2">
+                <NuxtLink to="/">Home</NuxtLink>
+            </li>
+            <li class="px-2">
                 <NuxtLink to="/contact">Contact</NuxtLink>
             </li>
         </ul>
