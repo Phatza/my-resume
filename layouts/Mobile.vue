086ef54573571dad9928d1896274104503da5b96
phatza <haphuoc@gmail.com>
2022-06-22 18:15:56 +0200

    Update: add SocialLinks component to the view

--- layouts/Mobile.vue
+++ layouts/Mobile.vue
@@ -1,11 +1,12 @@
 <template>
-    <div class="container px-5 flex flex-col justify-center content-center">
-        <header role="navigation">
-            <Profile />
-            <Nav />
-        </header>
-        <main>
-            <slot />
-        </main>
-    </div>
+  <div class="container px-5 flex flex-col justify-center items-center">
+    <header>
+      <Profile />
+      <Nav />
+      <SocialLinks />
+    </header>
+    <main>
+      <slot />
+    </main>
+  </div>
 </template>
