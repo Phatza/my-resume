1c985a76b31e0f2f036f7fb79bf256a130083cb0
phatza <haphuoc@gmail.com>
2022-06-24 15:22:49 +0200

    Update: skills components + index page)

--- components/Skills.vue
+++ components/Skills.vue
@@ -1,32 +1,52 @@
 <template>
   <div class="Skillsbars_container">
-    <div class="skillsBars_item py-2" v-for="item of skills">
-      <div class="flex justify-between">
-        <span class="text-base font-medium text-blue-700 dark:text-white">
-          {{ item.name }}
-        </span>
-        <span class="text-sm font-medium text-blue-700 dark:text-white"
-          >{{ item.score }}
-        </span>
+    <div class="Skillsbars_tech">
+      <h3>Techniques</h3>
+      <div class="skillsBars_tech-item py-2" v-for="item in techSkills">
+        <div class="flex justify-between">
+          <span class="text-base font-medium text-blue-700 dark:text-white">
+            {{ item.name }}
+          </span>
+          <span class="text-sm font-medium text-blue-700 dark:text-white"
+            >{{ item.score }}
+          </span>
+        </div>
+        <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+          <div
+            class="progress bg-blue-600 h-2.5 rounded-full transition-all transition-1000 delay-500 ease-in-out"
+            :style="'width:' + item.score + ';'"
+          ></div>
+        </div>
       </div>
-      <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
-        <div
-          class="progress bg-blue-600 h-2.5 rounded-full transition-all transition-1000 delay-500 ease-in-out"
-          :style="'width:' + item.score + ';'"
-        ></div>
+      <h3>Autres</h3>
+      <div class="skillsBars_tech-item py-2" v-for="item in othersSkills">
+        <div class="flex justify-between">
+          <span class="text-base font-medium text-blue-700 dark:text-white">
+            {{ item.name }}
+          </span>
+          <span class="text-sm font-medium text-blue-700 dark:text-white"
+            >{{ item.score }}
+          </span>
+        </div>
+        <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-700">
+          <div
+            class="progress bg-blue-600 h-2.5 rounded-full transition-all transition-1000 delay-500 ease-in-out"
+            :style="'width:' + item.score + ';'"
+          ></div>
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
-const skills = ref([
+const techSkills = ref([
   {
-    name: "HTML5",
+    name: "HTML",
     score: "95%",
   },
   {
-    name: "CSS3",
+    name: "CSS",
     score: "95%",
   },
   {
@@ -41,5 +61,29 @@ const skills = ref([
     name: "Nuxt",
     score: "80%",
   },
+  {
+    name: "NodeJS",
+    score: "60%",
+  },
+  {
+    name: "Strapi",
+    score: "70%",
+  },
+]);
+const othersSkills = ref([
+  {
+    name: "Français",
+    score: "100%",
+  },
+  {
+    name: "Anglais",
+    score: "80%",
+  }
 ]);
 </script>
+
+<style scoped lang="postcss">
+h3 {
+  @apply underline text-lg my-2;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,39 @@
 <template>
   <div>
     <section class="py-2">
-      <h1 class="font-bold text-3xl">About me</h1>
+      <h1 class="font-bold text-3xl">À propos de moi</h1>
       <div class="description py-4">
         <p>
-          Hey there 👋
+          Salut 👋
+          <!-- Hey there 👋 -->
           <br />
-          I'm Brice, a self-taught Front-end developer based in Marseille -
-          France.
+          Je m'appelle Brice et je suis développeur web (autodidacte) basé
+          sur Marseille - France.
+          <!-- I'm Brice, a self-taught Front-end developer
+          based in Marseille - France. -->
         </p>
         <p>
-          <span class="font-bold"
-            >I'm currently looking for a new challenge as Freelance within a
-            team based on VueJS, NuxtJS.</span
-          >
+          <span class="font-bold">
+            Je suis actuellement à la recherche d'un nouveau challenge en tant que freelance, au sein d'une équipe basé sur VueJS/NuxtJS.
+            <!-- I'm currently looking for a new challenge as Freelance within a team
+            based on VueJS, NuxtJS. -->
+          </span>
         </p>
         <p>
-          If you want to know more about me you can discover
-          <NuxtLink to="/experiences">my experiences</NuxtLink>
-          .
+          Si vous voulez en savoir plus sur moi, vous pouvez consulter
+          <NuxtLink class="underline" to="/experiences">mes expériences</NuxtLink>
+          <!-- If you want to know more about me you can discover
+          <NuxtLink to="/experiences">my experiences</NuxtLink> -->
+          . 
         </p>
-        <p>Feel free to reach me using my social links.</p>
+        <p>
+          N'hésitez pas à me contacter en utilisant les liens vers mes réseaux sociaux.
+          <!-- Feel free to reach me using my social links. -->
+        </p>
       </div>
     </section>
     <section>
-      <h2 class="font-bold text-xl">Discover my skills</h2>
+      <h2 class="font-bold text-xl mb-2">Découvrez mes compétences</h2>
       <Skills />
     </section>
   </div>
