cf0c7170c6395c567d3b92fd1ab9a5054c3cf00a
phatza <haphuoc@gmail.com>
2022-06-27 17:42:50 +0200

    Update: add divide bar between profile and navigation components

--- components/Profile.vue
+++ components/Profile.vue
@@ -5,9 +5,9 @@
                 <img src="~/assets/images/logo-phatza-creation-103x94.png" class="mx-auto shadow-lg rounded-1 shadow-slate-500" alt="avatar" />
             </NuxtLink>
         </div>
-        <div class="profile_name flex flex-col items-center">
+        <div class="profile_name flex flex-col items-center divide-y-4 divide-light-50 divide-y-reverse">
             <span class="font-bold text-xl">Brice HA PHUOC CHI</span>
-            <span class="font-bold text-lg">Front-end developer</span>
+            <span class="font-bold text-lg pb-2">Front-end developer</span>
         </div>
     </div>
 </template>
