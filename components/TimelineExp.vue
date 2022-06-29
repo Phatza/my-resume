9bd218fda9c7c5e71d135623da308ba0123661eb
phatza <haphuoc@gmail.com>
2022-06-29 18:10:17 +0200

    Update: experiences page style

--- components/TimelineExp.vue
+++ components/TimelineExp.vue
@@ -1,67 +1,79 @@
 <template>
-  <div class="timelineExp_container relative p-8 bg-white shadow-lg">
+  <div class="timelineExp_container relative md:p-8 bg-white shadow-lg">
     <div class="timelineExp_list">
       <ol class="relative border-l border-gray-200 dark:border-gray-700">
         <li
           v-if="experiences && experiences.length > 0"
-          v-for="item in experiences"
-          :key="item.id"
-          class="mb-10 ml-6"
+          v-for="(item, index) in experiences"
+          :key="index"
+          class="bg-light-200"
         >
-          <span
-            class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"
-          >
-            <nuxt-icon :name="item.icon" fill></nuxt-icon>
-          </span>
-          <h2
-            class="flex items-center mb-1 text-lg font-semibold text-gray-900 dark:text-white"
-          >
-            {{ item.title }}
-          </h2>
-          <span
-            class="block underline mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
-          >
-            {{ item.company }}
-          </span>
-          <time
-            class="block mb-2 text-sm font-normal italic leading-none text-gray-400 dark:text-gray-500"
-          >
-            {{ item.date }}
-          </time>
-          <div class="timelineExp_description">
-            <div v-if="item.toggleButton === false">
-              <p
-                v-html="item.excerpt"
-                class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"
-              ></p>
-              <button
-                class="readMore text-gray-900"
-                @click="item.toggleButton = !item.toggleButton"
-              >
-                Lire plus
-              </button>
+          <div class="mb-3 ml-3 p-4 md:mb-6 md:ml-6 md:p-5">
+            <span
+              class="flex absolute -left-3 justify-center items-center w-6 h-6 bg-blue-200 rounded-full ring-8 ring-white dark:ring-gray-900 dark:bg-blue-900"
+            >
+              <nuxt-icon :name="item.icon" fill></nuxt-icon>
+            </span>
+            <h2
+              class="flex underline mb-1 text-lg font-semibold text-gray-900 dark:text-white"
+            >
+              {{ item.title }}
+            </h2>
+            <span
+              class="block underline mb-2 text-sm font-normal leading-none text-gray-400 dark:text-gray-500"
+            >
+              {{ item.company }}
+            </span>
+            <time
+              class="block mb-2 text-sm font-normal italic leading-none text-gray-400 dark:text-gray-500"
+            >
+              {{ item.date }}
+            </time>
+            <div class="timelineExp_description">
+              <div v-if="item.toggleButton === false">
+                <p
+                  v-html="item.excerpt"
+                  class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"
+                ></p>
+                <button
+                  class="readMore text-gray-900"
+                  @click="item.toggleButton = !item.toggleButton"
+                >
+                  Lire plus
+                </button>
+              </div>
+              <div v-else class="timelineExp_description">
+                <p
+                  v-html="item.description"
+                  class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"
+                ></p>
+                <button
+                  class="readMore text-red-500"
+                  @click="item.toggleButton = !item.toggleButton"
+                >
+                  Lire moins
+                </button>
+              </div>
             </div>
-            <div v-else class="timelineExp_description">
-              <p
-                v-html="item.description"
-                class="mb-4 text-base font-normal text-gray-500 dark:text-gray-400"
-              ></p>
-              <button
-                class="readMore text-red-500"
-                @click="item.toggleButton = !item.toggleButton"
-              >
-                Lire moins
-              </button>
-            </div>
+            <a
+              v-if="item.url.length > 0"
+              :href="item.url"
+              target="_blank"
+              class="py-2 px-2 bg-dark-50 text-white rounded text-sm font-medium"
+            >
+              Voir le site
+            </a>
           </div>
-          <a
-            v-if="item.url.length > 0"
-            :href="item.url"
-            target="_blank"
-            class="py-2 px-2 bg-slate-100 rounded text-sm font-medium"
-          >
-            Voir le site
-          </a>
+        </li>
+        <li v-else class="text-gray-500">
+          <p>
+            Une erreur est survenue lors de la récupération des expériences.
+          </p>
+          <p>
+            Merci de retourner sur la
+            <NuxtLink to="/" class="underline">page d'accueil</NuxtLink>
+            avant de revenir sur cette page.
+          </p>
         </li>
       </ol>
     </div>
@@ -72,12 +84,16 @@
 const { data: experiences } = await useFetch("/api/experiences");
 // create exceptions for each experience description
 // and toggle system to show/hide excerpt/description
-experiences.value.forEach((item) => {
-  if (item.description.length > 100) {
-    item.toggleButton = false;
-    item.excerpt = item.description.substring(0, 100) + "...";
-  }
-});
+if (experiences.value.length > 0) {
+  experiences.value.forEach((item) => {
+    if (item.description.length > 100) {
+      item.toggleButton = false;
+      item.excerpt = item.description.substring(0, 100) + "...";
+    }
+  });
+} else {
+  experiences.value = [];
+}
 </script>
 
 <style scoped lang="postcss">
--- pages/experiences.vue
+++ pages/experiences.vue
@@ -1,6 +1,6 @@
 <template>
   <section class="relative bg-white">
-    <div class="exp_title bg-white shadow-md shadow-light-900 sticky top-0 left-0 right-0 z-10 p-5">
+    <div class="exp_title bg-white shadow-md shadow-light-900 sticky top-0 left-0 right-0 z-20 p-5">
       <h1 class="font-bold text-3xl">Découvrez mes expériences</h1>
     </div>
     <TimelineExp />
