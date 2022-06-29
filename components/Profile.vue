3abb718314f6c346649d735b1006da4ff54b7051
phatza <haphuoc@gmail.com>
2022-06-29 18:11:52 +0200

    Update: remove profile image and update style

--- components/Profile.vue
+++ components/Profile.vue
@@ -2,12 +2,18 @@
     <div class="profile_container p-5">
         <div class="profile_image mb-4">
             <NuxtLink to="/">
-                <img src="~/assets/images/logo-phatza-creation-103x94.png" class="mx-auto shadow-lg rounded-1 shadow-slate-500" alt="avatar" />
+                <img src="~/assets/images/brice-hpc-avatar.png" width="100" class="mx-auto shadow-lg rounded-1 shadow-slate-500" alt="avatar" />
             </NuxtLink>
         </div>
         <div class="profile_name flex flex-col items-center divide-y-4 divide-light-50 divide-y-reverse">
-            <span class="font-bold text-xl">Brice HA PHUOC CHI</span>
-            <span class="font-bold text-lg pb-2">Front-end developer</span>
+            <span class="font-bold text-white text-xl">Brice HA PHUOC CHI</span>
+            <span class="font-bold text-white text-lg pb-2">Développeur Web</span>
         </div>
     </div>
 </template>
+
+<style>
+.profile_name {
+    text-shadow: 0px 0px 10px rgba(0, 0, 0, 0.5);
+}
+</style>
